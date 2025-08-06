466b2d2ed57c1f6f04291085bbc55666c77a6925
wjzlskxk <wjzlskxk@users.noreply.github.com>
2025-08-06 17:22:39 +0000

    deploy: b2993476030f9b783be37b4e32f5ee0bb316013b

--- src/config/email.config.ts
+++
@@ -1,7 +0,0 @@
-const config = {
-  service_id: process.env.REACT_APP_EMAILJS_SERVICE_ID!,
-  template_id: process.env.REACT_APP_EMAILJS_TEMPLATE_ID!,
-  public_key: process.env.REACT_APP_EMAILJS_PUBLIC_KEY!,
-}
-
-export default config
